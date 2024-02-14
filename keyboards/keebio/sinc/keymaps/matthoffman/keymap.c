6b1ce03cb0866856c0a40aebb806ec610ffeb93a
Matt Hoffman <matt@matthoffman.org>
2024-02-14 08:58:53 -0500

    I assume these are what I'm using. They've been uncommitted for a while.

--- keyboards/keebio/sinc/keymaps/matthoffman/keymap.c
+++ keyboards/keebio/sinc/keymaps/matthoffman/keymap.c
@@ -3,18 +3,18 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_80_with_macro(
     KC_MUTE,          KC_ESC,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  LGUI(KC_0),
-    KC_MPLY, KC_MSTP, KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
-    KC_MPRV, KC_MNXT, KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
+    KC_MPLY, KC_MNXT, KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
+    C(S(KC_D)), C(S(KC_M)), KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
     KC_F13,  KC_F14,  RCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     KC_F15,  KC_F16,  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_F17,  KC_F18,  KC_LCTL,        KC_LALT, KC_LGUI, LT(1, KC_ESC),   XXXXXXX, LT(1, KC_SPC),   KC_SPC,  LT(1, KC_ESC),   KC_MEH,  KC_HYPR, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F17,  KC_F18,  KC_LCTL,        KC_LALT, KC_LGUI, KC_SPC,   XXXXXXX, LT(1, KC_ESC),   KC_SPC,  LT(1, KC_ESC),   KC_MEH,  KC_HYPR, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_80_with_macro(
     RGB_TOG,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    RGB_HUI, RGB_HUD, QK_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
-    RGB_SAI, RGB_SAD, RGB_TOG, RGB_MOD, _______, _______, _______, _______, SGUI(KC_LBRC), SGUI(KC_RBRC), _______, _______, _______, _______, _______, QK_BOOT,   _______,
-    RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,         _______, _______,
-    _______, _______, _______,          _______, _______, _______, _______, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LGUI(KC_DEL), _______, _______, _______, _______, _______,
+    KC_MSTP, KC_MPRV, QK_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
+    RGB_HUI, RGB_HUD, RGB_TOG, RGB_MOD, _______, _______, _______, _______, SGUI(KC_LBRC), SGUI(KC_RBRC), _______, _______, _______, _______, _______, QK_BOOT,   _______,
+    RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,         _______, _______,
+    RGB_VAI, RGB_VAD, _______,          _______, _______, _______, _______, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LGUI(KC_DEL), _______, _______, _______, _______, _______,
     QK_BOOT, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   ),
 };
